ba1ee03507dc61a2bc4b95196621ad593989de16
xnikoloff <hristiyan.at.nikoloff@gmail.com>
2023-02-08 18:11:28 +0200

    Add tests

---
+++ OwlStock.Tests/DataSeeder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OwlStock.Domain;
+using OwlStock.Infrastructure;
+
+namespace OwlStock.Tests
+{
+    public class DataSeeder
+    {
+        protected OwlStockDbContext BuildDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OwlStockDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            OwlStockDbContext context = new(optionsBuilder);
+            return context;
+
+        }
+
+        public async Task<OwlStockDbContext> ArrangeDbContext()
+        {
+            OwlStockDbContext context = BuildDbContext();
+
+            var photos = new List<Photo>
+            {
+                new Photo
+                {
+                    Name = "Test Photo 1",
+                    Description = null
+                }
+            };
+
+            await context.AddRangeAsync(photos);
+            await context.SaveChangesAsync();
+            return context;
+        }
+    }
+}
---
+++ OwlStock.Tests/PhotoTests/CreatePhotoTests.cs
@@ -0,0 +1,59 @@
+﻿using OwlStock.Services;
+using OwlStock.Services.Interfaces;
+using Xunit;
+using OwlStock.Domain;
+using OwlStock.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using OwlStock.Services.DTOs;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace OwlStock.Tests.PhotoTests
+{
+    public class CreatePhotoTests
+    {
+        [Fact]
+        public async Task CreatePhoto_WithCorrectData_ShouldCreatePhoto()
+        {
+            //Arrage
+            DataSeeder seeder = new();
+            OwlStockDbContext context = await seeder.ArrangeDbContext();
+            
+            byte[] bytes = await File.ReadAllBytesAsync("./testImage.jpg");
+            MemoryStream stream = new(bytes);
+
+            var formFile = new FormFile(stream, 0, stream.Length, "test", "testImage")
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "image/jpeg"
+            };
+
+
+            CreatePhotoDTO dto = new()
+            {
+                Name = "Test Photo",
+                Description = "Test Descr",
+                FormFile = formFile,
+                WebRootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+            };
+
+            IPhotoService service = new PhotoService(context);
+
+            //Act
+            int recordsCountBefore = 0;
+
+            if(context.Photos is not null)
+            {
+                recordsCountBefore = await context.Photos.CountAsync();
+            }
+
+            await service.Create(dto);
+
+            int recordsCountAfter = await context.Photos.CountAsync();
+
+            //Assert
+            Assert.True(recordsCountAfter == ++recordsCountBefore);
+        }
+    }
+}
