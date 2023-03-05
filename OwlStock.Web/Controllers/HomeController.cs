122fb68985ddab4e65f17968d1c942b4bf15cec5
xnikoloff <hristiyan.at.nikoloff@gmail.com>
2023-03-05 18:25:50 +0200

    add photo grid

--- OwlStock.Web/Controllers/HomeController.cs
+++ OwlStock.Web/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OwlStock.Services.DTOs;
+using OwlStock.Services.Interfaces;
 
 namespace OwlStock.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IPhotoService _photoService;
+
+        public HomeController(IPhotoService photoService)
         {
-            return View();
+            _photoService = photoService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<AllPhotosDTO> dto = await _photoService.All();
+            return View(dto.TakeLast(12).ToList());
         }
     }
 }
