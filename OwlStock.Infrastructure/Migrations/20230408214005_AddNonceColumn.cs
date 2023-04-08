a9224731cfa090636c37d3065fbc6d35c857603e
xnikoloff <hristiyan.at.nikoloff@gmail.com>
2023-04-09 01:49:42 +0300

    Fix empty nonce

---
+++ OwlStock.Infrastructure/Migrations/20230408214005_AddNonceColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OwlStock.Infrastructure.Migrations
+{
+    public partial class AddNonceColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nonce",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nonce",
+                table: "Orders");
+        }
+    }
+}
