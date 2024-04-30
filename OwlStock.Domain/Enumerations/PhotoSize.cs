a69b5ffe0ab45b77c51e33a9461a2f6aab5a745e
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2024-05-01 01:24:06 +0300

    translate static texts

--- OwlStock.Domain/Enumerations/PhotoSize.cs
+++ OwlStock.Domain/Enumerations/PhotoSize.cs
@@ -1,10 +1,19 @@
-﻿namespace OwlStock.Domain.Enumerations
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OwlStock.Domain.Enumerations
 {
     public enum PhotoSize
     {
+        [Display(Name = "Пълен размер")]
         OriginalSize = 1,
+
+        [Display(Name = "Малка")]
         Small = 2,
+
+        [Display(Name = "Средна")]
         Medium = 3,
+
+        [Display(Name = "Голяма")]
         Large = 4
     }
 }
