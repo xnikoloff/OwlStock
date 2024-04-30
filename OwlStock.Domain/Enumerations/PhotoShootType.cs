3a7f806e0f4129eb385b7c1f319e82369c67bd52
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2024-05-01 00:38:31 +0300

    change enum display name

--- OwlStock.Domain/Enumerations/PhotoShootType.cs
+++ OwlStock.Domain/Enumerations/PhotoShootType.cs
@@ -1,13 +1,31 @@
-﻿namespace OwlStock.Domain.Enumerations
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OwlStock.Domain.Enumerations
 {
     public enum PhotoShootType
     {
+        [Display(Name = "Лична фотосесия")]
         Personal = 1,
+
+        [Display(Name = "Сватба")]
         Wedding = 2,
+
+        [Display(Name = "Лично празненство")]
         Party = 3,
+
+        [Display(Name = "Детска фотосесия")]
         Kids = 4,
+
+        [Display(Name = "Имот/сграда")]
         Property = 5,
+
+        [Display(Name = "Събитие")]
         Event = 6,
-        Other = 7
+
+        [Display(Name = "Автомобил")]
+        Automotive = 7,
+
+        [Display(Name = "Друг")]
+        Other = 8
     }
 }
